7ec7c3feb839ee4ef7522eb7557e0769a3daf23f
pr293504 <pr293504@wipro.com>
2018-10-25 14:53:47 +0530

    with heroku build

--- src/app/favourites.service.ts
+++ src/app/favourites.service.ts
@@ -8,24 +8,24 @@ export class FavouritesService {
     constructor(private http: Http) { };
 
     getMovies () {
-        const url = `http://localhost:4000/api/favourites/`;
+        const url = `https://illuminatimovieapp.herokuapp.com/api/favourites/`;
         return this.http.get(url).map(res => res.json());
     }
     postMovies (movie) {
-        const url = `http://localhost:4000/api/favourites/`;
+        const url = `https://illuminatimovieapp.herokuapp.com/api/favourites/`;
         return this.http.post(url, movie);
     }
     checkStatus(movie) {
-    const url = `http://localhost:4000/api/favourites/${movie}`;
+    const url = `https://illuminatimovieapp.herokuapp.com/api/favourites/${movie}`;
         return this.http.get(url).map(res => res.json());
     }
     deleteMovies (movie) {
-        const url = `http://localhost:4000/api/favourites/${movie}`;
+        const url = `https://illuminatimovieapp.herokuapp.com/api/favourites/${movie}`;
         return this.http.delete(url).map(res => res.json());
     }
     updateMovies (movie) {
         const headers = new Headers({ 'Content-Type': 'application/json;charset=utf-8' });
-        const url = `http://localhost:4000/api/favourites/${movie.id}`;
+        const url = `https://illuminatimovieapp.herokuapp.com/api/favourites/${movie.id}`;
         return this.http.put(url, movie, headers);
     }
 }
